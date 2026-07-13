91b752f9a5e418d732aa7e86928501e7666a4d8c
Neha Susan Lijo <146093395+nehalijo@users.noreply.github.com>
2026-07-14 00:23:20 +0530

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if (s1.size() > s2.size()) return false;
+        int l = 0;
+        int r = s1.size() - 1;
+        vector<int> freq1(26, 0);
+        vector<int> freq2(26, 0);
+        for (int i = 0; i < s1.size(); i++) {
+            freq1[s1[i] - 'a']++;
+            freq2[s2[i] - 'a']++;
+        }
+        while (r < s2.size()) {
+            if (freq1 == freq2) return true;
+            freq2[s2[l] - 'a']--;
+            l++;
+            r++;
+            if (r < s2.size()) {
+                freq2[s2[r] - 'a']++;
+            }
+        }
+        return false;
+    }
+};
