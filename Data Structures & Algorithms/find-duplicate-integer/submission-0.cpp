52f4818bfc2931994fa460badc63e756cc300e07
Neha Susan Lijo <146093395+nehalijo@users.noreply.github.com>
2026-09-23 22:43:31 +0530

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+        for (int n: nums) {
+            if (seen.count(n)) return n;
+            seen.insert(n);
+        }
+        return -1;
+    }
+};
